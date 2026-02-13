2b0b8d98ab3aadc82a202752a426b45176e792db
Georges <georgeshibou357@gmail.com>
2026-02-13 12:16:16 +0100

    debut code

---
+++ test.py
@@ -0,0 +1,20 @@
+from Classe_pile import *
+import pyxel
+
+
+
+def test_entree(lab):
+    """ teste si il existe une entree
+    E: lab(labyrinthe)
+    S: bool"""
+    for case in labyrinthe():
+        if case == "X":
+            rzjouterjknbljk
+            return False
+        
+lab = [[(0,0,X),(1,0,X),(2,0,X),(3,0,X),(4,0,X)]
+       [(0,1,X),(1,1, ),(2,1,X),(3,1, ),(4,1,S)]
+       [(0,2,X),(1,2, ),(2,2, ),(3,2, ),(4,2,X)]
+       [(0,3,X),(1,3,E),(2,3,X),(3,3, ),(4,3,X)]
+       [(0,4,X),(1,4, ),(2,4,X),(3,4, ),(4,4,X)]
+       [(0,5,X),(1,5,X),(2,5,X),(3,5,X),(4,5,X)]]
