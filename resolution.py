1fdf5aec77f7e931757946300b5343bbee5edd21
Houmous106 <georgeshibou357@gmail.com>
2026-03-01 22:43:36 +0100

    fin du code de résolution

--- resolution.py
+++ resolution.py
@@ -1,6 +1,6 @@
 
-from Classe_pile import Pile
-from test import test, bordure
+from Classe_pile import *
+from test import *
 
 
 labyrinthe = [[[0,0,"X"],[0,1,"X"],[0,2,"X"],[0,3,"X"],[0,4,"X"]],
@@ -10,36 +10,83 @@ labyrinthe = [[[0,0,"X"],[0,1,"X"],[0,2,"X"],[0,3,"X"],[0,4,"X"]],
               [[4,0,"X"],[4,1," "],[4,2,"X"],[4,3," "],[4,4,"X"]],
               [[5,0,"X"],[5,1,"X"],[5,2,"X"],[5,3,"X"],[5,4,"X"]]]
 
-def entree():  
+
+
+def entree(lab):
+    """Trouve l'entrée du labyrinthe à résoudre
+    E = lab le labyrinthe sous forme de liste de listes de cases formant une matrice
+    S = un tuple avec les coordonnées de l'entrée"""
     for ligne in lab:
         for case in ligne:
             if case[2] == "E":
                 return (case[0], case[1])
 
+
+
 def reso(C, lab):
-    """Résolution d'un labyrinthe en partant d'une"""
-    assert type(C) == tuple
-    assert type(lab) == list
+    """Résolution d'un labyrinthe en partant d'une case
+    E = C la case et lab le labyrinthe en format texte
+    S = le labyrinthe avec le trajet tracé avec des points"""
 
-    if lab[C[0]][C[1]][2] == "S":
-        return lab
-    else:
-        lab[C[0]][C[1]][2] = "."
-
-        if lab[C[0]-1][C[1]][2] == " " or lab[C[0]-1][C[1]][2] == "S":
-            return reso((C[0]-1,C[1]), lab)
-
-        elif lab[C[0]-1][C[1]][2] == " " or lab[C[0]-1][C[1]][2] == "S":
-            return reso((C[0]-1,C[1]), lab)
-
-        elif lab[C[0]-1][C[1]][2] == " " or lab[C[0]-1][C[1]][2] == "S":
-            return reso((C[0]-1,C[1]), lab)
-
-        elif lab[C[0]-1][C[1]][2] == " " or lab[C[0]-1][C[1]][2] == "S":
-            return reso((C[0]-1,C[1]), lab)
-
-#        else:
+    if lab[C[0]][C[1]][2] == "S": # condition d'arret : on est sur la case S
+        return True
 
 
+    lab[C[0]][C[1]][2] = "." # les cases sur lequelles ont est deja passé sont marquées d'un point
 
-#if 
+    if lab[C[0]-1][C[1]][2] == " " or lab[C[0]-1][C[1]][2] == "S": # on se balade dans le labyrinthe jusqu'a tomber sur la case S
+        if reso((C[0]-1,C[1]), lab):
+            return True
+
+    if lab[C[0]+1][C[1]][2] == " " or lab[C[0]+1][C[1]][2] == "S":
+        if reso((C[0]+1,C[1]), lab):
+            return True
+
+    if lab[C[0]][C[1]-1][2] == " " or lab[C[0]][C[1]-1][2] == "S":
+        if reso((C[0],C[1]-1), lab):
+            return True
+
+    if lab[C[0]][C[1]+1][2] == " " or lab[C[0]][C[1]+1][2] == "S":
+        if reso((C[0],C[1]+1), lab):
+            return True
+
+
+    lab[C[0]][C[1]][2] = " " # c'est une impasse donc on retire le point
+    return False
+
+
+
+def connection(lab):
+    """Vérifie que l'entrée et la sortie sont bien connectées en vérifiant que reso() a bien laissé au moins 1 point
+    E = lab le labyrinthe modifié ou non par reso
+    S = booleen, True si l'entrée et la sortie sont connectées, False sinon"""
+    for ligne in lab:
+            for case in ligne:
+                if case[2] == ".":
+                    return True
+    return False
+
+
+
+def solution(lab):
+    """Affichage propre de la solution d'un labyrinthe
+    E = lab le labyrinthe sous forme de liste de liste de cases formant une matrice
+    S = sol le labyrinthe avec le chemin vers la sortie marqué pas des points"""
+
+    if test(lab) == True and bordure(lab) == True:
+
+        sol = lab
+        E = entree(sol)
+        reso(E, sol)
+        if connection(sol):
+            for ligne in sol:
+                print(ligne)
+            return sol
+
+    print("Ce labyrinthe n'est pas solvable")
+    return lab
+
+
+
+
+solution(labyrinthe)
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ def test(lab):
     entree = 0
     sortie = 0
     larg = lab[0]    #nombre de case dans ligne (on utilise l'indice 0 car toutes les  lignes sont de la meme longueur)
+
     for i in range(len(lab)-1):  #nombre de ligne dans lab
         for j in range(len(larg)):    
             case = lab[i][j][2]
@@ -24,20 +25,11 @@ def test(lab):
             elif case == "E":
                 entree = entree + 1  #determine s'il y a une entree 
                 coor_entree.append(lab[i][j][:2])
-    print("voici les cases pleines : ")
-    pleine.afficherp()
-    print("voici les cases vides : ")
-    vide.afficherp()
+
     if entree != 0 or entree == sortie:
-        print("Il y a",entree,"entree en", coor_entree,"et",sortie, "sortie en", coor_sortie, "!")
         return True
+
     else:        
-        if entree == sortie and entree == 0:
-            print("Il n'y a pas d'entree et pas de sortie !")
-        elif entree != 1:
-            print("Il y a ", entree, "entree(s) !")
-        elif sortie != 1:
-            print("Il y a", sortie,"sortie(s) !")
         return False
     
 
@@ -47,6 +39,7 @@ def bordure(lab):
     S: bool"""
     bordures = []
     vides = []
+
     for ligne in range(len(lab[0])):
         bordures.append(lab[0][ligne]) 
         bordures.append(lab[-1][ligne])
@@ -57,6 +50,7 @@ def bordure(lab):
 
     total_bord = len(bordures) 
     bords = 0    
+
     for i in range(len(bordures)):  
         if bordures[i][2] == "X":
             bords =+ 1
@@ -69,18 +63,7 @@ def bordure(lab):
             vides.append(bordures[i][:2])
 
     if vides == []:
-        print("La bordure est parfaite !")
         return True
+
     else:
-        print("La bordure est vide en : ", vides, "!")
         return False
-
-labyrinthe =[[[0,0,"X"],[0,1,"X"],[0,2,"X"],[0,3,"X"],[0,4,"X"]],
-             [[1,0,"X"],[1,1," "],[1,2,"X"],[1,3," "],[1,4,"S"]],
-             [[2,0,"X"],[2,1," "],[2,2," "],[2,3," "],[2,4,"X"]],
-             [[3,0,"X"],[3,1,"E"],[3,2,"X"],[3,3," "],[3,4,"X"]],
-             [[4,0,"X"],[4,1," "],[4,2,"X"],[4,3," "],[4,4,"X"]],
-             [[5,0,"X"],[5,1,"X"],[5,2,"X"],[5,3,"X"],[5,4,"X"]]]
-
-print(test(labyrinthe))
-print(bordure(labyrinthe))
