b6c410cf90cbc0200f2aee470454888e56cbf257
Georges <georgeshibou357@gmail.com>
2026-03-04 12:02:33 +0100

    on se deplace sur le pc de mathis car j'ai pas pyxel

--- launcher.py
+++ launcher.py
@@ -1,3 +1,4 @@
+import pyxel
 from test import *
 from Classe_pile import *
 from txt_to_list import *
@@ -5,4 +6,9 @@ from resolution import *
 from interface_A import *
 
 def launch(lab):
-    listificator(lab)
+    laby_list = listificator(lab)
+    sol = solution(laby_list)
+    return Labyrinthe(laby_list)
+
+
+launch("dedale.txt")
